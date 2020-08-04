8ac526f9c23230d40fd6706ac94297da6619d5b0
Janet Bah <jadhiambo20@gmail.com>
2020-08-04 10:14:47 -0500

    Completed the queries

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,12 @@
 --Part 1
+SELECT * FROM jobs
 
 --Part 2
+SELECT * FROM Employers where employers.Location = "St Louis";
+insert into employers (Name, Location) values ("exScripts", "St Louis");
 
 --Part 3
+SELECT Name, Description FROM skills
+WHERE name  is not null
+order by name ASC;
 
